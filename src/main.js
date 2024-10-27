57cea91590b0ab930575ead394717f3e674cb22e
JumMy777 <jummy1806@gmail.com>
2024-10-28 05:37:59 +0800

    +added icons

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
+import '@mdi/font/css/materialdesignicons.css'
 // Vuetify
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
