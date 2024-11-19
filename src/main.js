fcaa74533e7100da58ac3a00f76e48bc69d7837c
JumMy777 <jummy1806@gmail.com>
2024-11-20 03:52:47 +0800

    added login and register vue

--- src/main.js
+++ src/main.js
@@ -2,12 +2,12 @@
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import '@mdi/font/css/materialdesignicons.css'
 // Vuetify
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+import '@mdi/font/css/materialdesignicons.css'
 
 import App from './App.vue'
 import router from './router'
@@ -15,6 +15,9 @@ import router from './router'
 const app = createApp(App)
 
 const vuetify = createVuetify({
+  icons: {
+    defaultSet: 'mdi', // This is already the default value - only for display purposes
+  },
   components,
   directives,
 })
